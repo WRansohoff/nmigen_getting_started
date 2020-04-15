67510259f7c38af2703b764d8b9422eac404fb4a
WRR <-@->
2020-04-14 20:18:58 -0400

    Add an initial Memory and FSM test design.

---
+++ hello_memory/rom.py
@@ -0,0 +1,79 @@
+from nmigen import *
+from math import ceil, log2
+from nmigen.back.pysim import *
+from nmigen_soc.memory import *
+from nmigen_soc.wishbone import *
+
+# Simulated read-only memory module.
+class ROM( Elaboratable, Interface ):
+  def __init__( self, data ):
+    # Record size.
+    self.size = len( data )
+    # Data storage.
+    self.data = Memory( width = 32, depth = self.size, init = data )
+    # Memory read port.
+    self.r = self.data.read_port()
+
+    # Initialize Wishbone bus interface.
+    Interface.__init__( self,
+                        data_width = 32,
+                        addr_width = ceil( log2( self.size + 1 ) ) )
+    self.memory_map = MemoryMap( data_width = self.data_width,
+                                 addr_width = self.addr_width,
+                                 alignment = 0 )
+
+  def elaborate( self, platform ):
+    m = Module()
+    # Register the read port submodule.
+    m.submodules.r = self.r
+
+    # 'ack' signal should rest 0.
+    m.d.sync += self.ack.eq( 0 )
+    # Simulated reads only take one cycle, but only acknowledge
+    # them after 'cyc' and 'stb' are asserted.
+    with m.If( self.cyc ):
+      m.d.sync += self.ack.eq( self.stb )
+
+    # Set 'dat_r' bus signal to the value in the
+    # requested 'data' array index.
+    m.d.comb += [
+      self.r.addr.eq( self.adr ),
+      self.dat_r.eq( self.r.data )
+    ]
+
+    # End of simulated memory module.
+    return m
+
+# Testbench:
+# Perform an individual 'memory read' unit test.
+def rom_read_ut( rom, address, expected ):
+  # Set address, and wait a tick.
+  yield rom.adr.eq( address )
+  yield Tick()
+  # Done. Check the result after combinatorial logic settles.
+  yield Settle()
+  actual = yield rom.dat_r
+  if expected == actual:
+    print( "PASS: Memory[ 0x%04X ] = 0x%08X"%( address, expected ) )
+  else:
+    print( "FAIL: Memory[ 0x%04X ] = 0x%08X (got: 0x%08X)"
+           %( address, expected, actual ) )
+
+# Run a basic series of tests when this file is run.
+if __name__ == "__main__":
+  # Create a test memory with 20 bytes of data.
+  dut = ROM( [ 0x01234567, 0x89ABCDEF,
+               0x0C0FFEE0, 0xDEC0FFEE,
+               0xFEEBEEDE ] )
+  # Run the simulation.
+  with Simulator( dut, vcd_file = open( 'rom.vcd', 'w' ) ) as sim:
+    def proc():
+      # Test reads.
+      yield from rom_read_ut( dut, 0, 0x01234567 )
+      yield from rom_read_ut( dut, 1, 0x89ABCDEF )
+      yield from rom_read_ut( dut, 2, 0x0C0FFEE0 )
+      yield from rom_read_ut( dut, 3, 0xDEC0FFEE )
+      yield from rom_read_ut( dut, 4, 0xFEEBEEDE )
+    sim.add_clock( 1e-6 )
+    sim.add_sync_process( proc )
+    sim.run()
---
+++ hello_memory/top.py
@@ -0,0 +1,143 @@
+from nmigen import *
+from nmigen.back.pysim import *
+from nmigen_boards.upduino_v2 import *
+from nmigen_soc.wishbone import *
+from nmigen_soc.memory import *
+
+from rom import *
+
+import sys
+
+# Dummy LED class for testing.
+class DummyLED():
+  def __init__( self, name ):
+    self.o = Signal( 1, reset = 0b0, name = '%s_o'%name )
+
+# Basic instruction definitions.
+# Red LED on/off
+RED_ON  = 0x00000009
+RED_OFF = 0x00000001
+# Green LED on/off
+GRN_ON  = 0x0000000A
+GRN_OFF = 0x00000002
+# Blue LED on/off
+BLU_ON  = 0x0000000B
+BLU_OFF = 0x00000003
+# Delay for 'xxxxxxx' cycles.
+def DELAY( cycles ):
+  return ( ( 0x4 | ( cycles << 4 ) ) & 0xFFFFFFFF )
+# Jump back to word 0.
+RETURN  = 0x00000000
+
+class Memory_Test( Elaboratable ):
+  def __init__( self, memory ):
+    # Simulated memory module.
+    self.mem = memory
+    # Current memory pointer.
+    self.pc = Signal( self.mem.addr_width, reset = 0 )
+    # Delay counter.
+    self.dc = Signal( 28, reset = 0 )
+
+  def elaborate( self, platform ):
+    m = Module()
+    m.submodules.mem = self.mem
+
+    # LED resources.
+    if platform is None:
+      rled = DummyLED( 'led_r' )
+      gled = DummyLED( 'led_g' )
+      bled = DummyLED( 'led_b' )
+    else:
+      rled = platform.request( 'led_r', 0 )
+      gled = platform.request( 'led_g', 0 )
+      bled = platform.request( 'led_b', 0 )
+
+    # Set bus address to the 'program counter' value.
+    m.d.comb += self.mem.adr.eq( self.pc )
+
+    # State machine:
+    # * 'FETCH':   Retrieve the next instruction from memory.
+    # * 'PROCESS': Execute the current instruction.
+    with m.FSM():
+      # 'FETCH' state: Get the next word from memory.
+      with m.State( 'FETCH' ):
+        # Pulse 'stb' and 'cyc' to start the bus transaction.
+        m.d.sync += [
+          self.mem.stb.eq( 1 ),
+          self.mem.cyc.eq( 1 )
+        ]
+        # Proceed once 'ack' is asserted.
+        with m.If( self.mem.ack == 1 ):
+          # Reset the delay counter, and clear 'stb' / 'cyc' to end
+          # the bus transaction. This also causes 'ack' to be cleared.
+          m.d.sync += [
+            self.dc.eq( 0 ),
+            self.mem.stb.eq( 0 ),
+            self.mem.cyc.eq( 0 )
+          ]
+          m.next = 'PROCESS'
+        # Read is ongoing while 'ack' is not asserted.
+        with m.Else():
+          m.next = 'FETCH'
+      # 'PROCESS' state: execute the retrieved instruction.
+      with m.State( 'PROCESS' ):
+        # Unless otherwise specified, increment the PC address
+        # and proceed back to the 'FETCH' state.
+        m.d.sync += self.pc.eq( self.pc + 1 )
+        m.next = 'FETCH'
+        # If the word is 0 or -1, reset PC address to 0 instead of
+        # incrementing it. 0xFFFFFFFF can indicate an error or
+        # uninitialized SPI memory, so it's a good 'return' trigger.
+        with m.If( ( self.mem.dat_r == 0x00000000 ) |
+                   ( self.mem.dat_r == 0xFFFFFFFF ) ):
+          m.d.sync += self.pc.eq( 0 )
+        # If the 4 LSbits equal 0x4, delay for a number of cycles
+        # indicated by the remaining 28 MSbits.
+        with m.Elif( self.mem.dat_r[ :4 ] == 4 ):
+          # If the delay has not finished, increment 'delay counter'
+          # without changing the PC address, and return to the
+          # 'PROCESS' state instead of moving on to 'NEXT'.
+          with m.If( self.dc != ( self.mem.dat_r >> 4 ) ):
+            m.d.sync += [
+              self.dc.eq( self.dc + 1 ),
+              self.pc.eq( self.pc )
+            ]
+            m.next = 'PROCESS'
+        # If the 3 LSbits == 3, set the blue LED to the 4th bit.
+        with m.Elif( self.mem.dat_r[ :3 ] == 3 ):
+          m.d.sync += bled.o.eq( self.mem.dat_r[ 3 ] )
+        # If the 3 LSbits == 2, set the green LED to the 4th bit.
+        with m.Elif( self.mem.dat_r[ :3 ] == 2 ):
+          m.d.sync += gled.o.eq( self.mem.dat_r[ 3 ] )
+        # If the 3 LSbits == 1, set the red LED to the 4th bit.
+        with m.Elif( self.mem.dat_r[ :3 ] == 1 ):
+          m.d.sync += rled.o.eq( self.mem.dat_r[ 3 ] )
+
+    # (End of memory and finite state machine test logic.)
+    return m
+
+# 'main' method to simulate or build the test.
+if __name__ == "__main__":
+  # If the file was run with '-b', build for an 'Upduino' board
+  if ( len( sys.argv ) == 2 ) and ( sys.argv[ 1 ] == '-b' ):
+    dut = Memory_Test( ROM( [
+      BLU_ON, DELAY( 500000 ), GRN_ON, DELAY( 500000 ),
+      BLU_OFF, RED_ON, DELAY( 1000000 ), RED_OFF,
+      DELAY( 500000 ), GRN_OFF, DELAY( 500000 ), RETURN
+    ] ) )
+    UpduinoV2Platform().build( dut )
+  # If no arguments were passed in, simulate the design.
+  else:
+    dut = Memory_Test( ROM( [
+      BLU_ON, DELAY( 5 ), GRN_ON, DELAY( 5 ), BLU_OFF, RED_ON,
+      DELAY( 10 ), RED_OFF, DELAY( 5 ), GRN_OFF, DELAY( 5 ), RETURN
+    ] ) )
+    with Simulator( dut, vcd_file = open( 'test.vcd', 'w' ) ) as sim:
+      # Simulate running for 200 clock cycles.
+      def proc():
+        for i in range( 200 ):
+          yield Tick()
+          yield Settle()
+      sim.add_clock( 1e-6 )
+      sim.add_sync_process( proc )
+      sim.run()
